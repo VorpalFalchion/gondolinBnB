aa0e57558c35ce65433e61de78af6c38641c9b45
VorpalFalchion <VorpalFalchion@hotmail.com>
2016-12-29 18:58:24 -0500

    first commit

---
+++ gondolinbnb/clientcolony/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ClientcolonyConfig(AppConfig):
+    name = 'clientcolony'
---
+++ gondvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\marmstrong\pythonprojects\gondolinroot\gondvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
