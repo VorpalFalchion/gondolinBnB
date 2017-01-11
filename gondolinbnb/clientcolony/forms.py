fbcc1e5725e1b6d591be0b7b98749e1c5cc503f1
VorpalFalchion <VorpalFalchion@hotmail.com>
2017-01-11 02:55:43 -0500

    More Auth, added form for New User

--- gondolinbnb/clientcolony/forms.py
+++ gondolinbnb/clientcolony/forms.py
@@ -1,6 +1,16 @@
 from django.contrib.auth.forms import AuthenticationForm
 from django import forms
+from django.contrib.auth.models import User
 
 class LoginForm(AuthenticationForm):
     username = forms.CharField(label='Username', max_length=30, widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'username'}))
     password = forms.CharField(label='Password', max_length=30, widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'password'}))
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username','first_name','email','password']
+        username = forms.CharField(label='Username', max_length=30, widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'username'}))
+        first_name = forms.CharField(label='Player Name', max_length=30, widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'first_name'}))
+        email = forms.CharField(label='email', max_length=50, widget=forms.EmailInput(attrs={'class': 'form-control', 'name': 'email'}))
+        password = forms.CharField(label='password', max_length=30, widget=forms.PasswordInput(attrs={'class': 'form-control', 'name': 'password'}))
--- gondolinbnb/clientcolony/views.py
+++ gondolinbnb/clientcolony/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
+from .forms import UserForm
 # Create your views here.
 @login_required(login_url="login/")
 
 def home(request):
-    return render(request, "home.html")
-#def login(request):
-#    return render(request, 'clientcolony/login.html')
+    return render(request, "clientcolony/home.html")
+
+def logout_view(request):
+    logout(request)
+    return redirect(request, 'login')
+
+def new_user(request):
+    form = UserForm()
+    return render(request, "clientcolony/new_user.html", {'form': form})
