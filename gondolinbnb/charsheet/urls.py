3cb307971e9f868f82d5967eb34581263da894ac
VorpalFalchion <VorpalFalchion@hotmail.com>
2017-01-16 04:30:42 -0500

    add edit button, edit does not save yet

--- gondolinbnb/charsheet/urls.py
+++ gondolinbnb/charsheet/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^charindex/$', views.index, name='index'),
     url(r'^edit/$', views.edit, name='edit'),
-    url(r'^charsheet/(?P<id>\w+)/', views.sheet, name='charsheet'),
+    url(r'^edit/(?P<id>\w+)/$', views.edit, name='edit'),
+    url(r'^charsheet/(?P<id>\w+)/$', views.sheet, name='charsheet'),
+    url(r'^create/$', views.create, name = 'create'),
 ]
--- gondolinbnb/charsheet/views.py
+++ gondolinbnb/charsheet/views.py
@@ -20,21 +20,43 @@ def index(request):
     }
     return render(request, 'charsheet/index.html', context)
 
-def edit(request):
-    registered = False
+def edit(request, id=''):
+    new_flag = False
+    character = ''
+    if id=='':
+        new_flag = True
+    else:
+        character = Character.objects.get(pk=id)
 
     if request.method == "POST":
-        #character_form = CharacterForm(data=request.POST)
-        #character_form.player_id = request.user
-
         form = CharacterForm(request.POST)
-        #form.player_id = request.user
 
+        if form.is_valid() and new_flag == True:
+            new_char = form.save(commit=False)
+            new_char.player_id = request.user.id
+            new_char.save()
+        elif form.is_valid() and new_flag == False:
+            character.save()
+
+    else:
+        if new_flag == True:
+            form = CharacterForm()
+        else:
+            form = CharacterForm(instance=character)
+
+    context = {
+        'character': character,
+        'form': form,
+    }
+    return render(request, "charsheet/edit.html", context)
+
+def create(request):
+    if request.method == "POST":
+        form = CharacterForm(request.POST)
         if form.is_valid():
             new_char = form.save(commit=False)
             new_char.player_id = request.user.id
             new_char.save()
-            registered = True
     else:
         form = CharacterForm()
-    return render(request, "charsheet/edit.html", {'form': form, 'registered': registered})
+    return render(request, "charsheet/create.html", {'form': form,})
