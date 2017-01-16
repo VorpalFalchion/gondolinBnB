8b2e245fea7f0160508ad8284365c1d4a42d0490
VorpalFalchion <VorpalFalchion@hotmail.com>
2017-01-16 18:42:49 -0500

    test2

--- gondvenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\marmstrong\pythonprojects\gondolinroot\gondvenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
