8a43bfc5a34a702da8c08def0ee8784b59ea500a
VorpalFalchion <VorpalFalchion@hotmail.com>
2017-01-20 17:39:23 -0500

    database shit

--- gondolinbnb/settings.py
+++ gondolinbnb/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-DATABASES['default'].update(db_from_env)
+DATABASES['default']. = dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
